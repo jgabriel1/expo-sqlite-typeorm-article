b0cbcff7a2ff1d0fd831828da119545af5b23372
jgabriel1 <josegabriel@det.ufc.br>
2020-12-17 12:45:20 -0300

    Create database connection React context.

---
+++ src/data/connection.tsx
@@ -0,0 +1,67 @@
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import { ActivityIndicator } from 'react-native';
+import { Connection, createConnection } from 'typeorm';
+
+import { TodoModel } from './entities/TodoModel';
+import { CreateTodosTable1608217149351 } from './migrations/1608217149351-CreateTodosTable';
+import { TodosRepository } from './repositories/TodosRepository';
+
+interface DatabaseConnectionContextData {
+  todosRepository: TodosRepository;
+}
+
+const DatabaseConnectionContext = createContext<DatabaseConnectionContextData>(
+  {} as DatabaseConnectionContextData,
+);
+
+export const DatabaseConnectionProvider: React.FC = ({ children }) => {
+  const [connection, setConnection] = useState<Connection | null>(null);
+
+  const connect = useCallback(async () => {
+    const createdConnection = await createConnection({
+      type: 'expo',
+      database: 'todos.db',
+      driver: require('expo-sqlite'),
+      entities: [TodoModel],
+
+      migrations: [CreateTodosTable1608217149351],
+      migrationsRun: true,
+
+      synchronize: false,
+    });
+
+    setConnection(createdConnection);
+  }, []);
+
+  useEffect(() => {
+    if (!connection) {
+      connect();
+    }
+  }, [connect, connection]);
+
+  if (!connection) {
+    return <ActivityIndicator />;
+  }
+
+  return (
+    <DatabaseConnectionContext.Provider
+      value={{
+        todosRepository: new TodosRepository(connection),
+      }}
+    >
+      {children}
+    </DatabaseConnectionContext.Provider>
+  );
+};
+
+export function useDatabaseConnection() {
+  const context = useContext(DatabaseConnectionContext);
+
+  return context;
+}
