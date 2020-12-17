1a2f1b48da7cc0cff187fc8a99389b23dece9f85
jgabriel1 <josegabriel@det.ufc.br>
2020-12-16 23:51:44 -0300

    Added necessary babel plugin.

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,9 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      '@babel/transform-react-jsx-source',
+      'babel-plugin-transform-typescript-metadata',
+    ]
   };
 };
