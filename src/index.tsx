67c52aa839474311f379642d3ca2cb92310892fc
jgabriel1 <josegabriel@det.ufc.br>
2020-12-17 12:49:12 -0300

    Provided the database context to the app.

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,16 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native';
 
 import TodoList from './components/TodoList';
+import { DatabaseConnectionProvider } from './data/connection';
 
 const App: React.FC = () => {
   return (
-    <>
+    <DatabaseConnectionProvider>
       <StatusBar style="auto" />
       <SafeAreaView style={{ flex: 1 }}>
         <TodoList />
       </SafeAreaView>
-    </>
+    </DatabaseConnectionProvider>
   );
 };
 
