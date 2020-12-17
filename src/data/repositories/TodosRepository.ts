7bd14ae70393bc50f7e66f39efcc070bc8aa97f4
jgabriel1 <josegabriel@det.ufc.br>
2020-12-17 12:44:13 -0300

    Create todo repository.

---
+++ src/data/repositories/TodosRepository.ts
@@ -0,0 +1,36 @@
+import { Connection, Repository } from 'typeorm';
+import { TodoModel } from '../entities/TodoModel';
+
+interface ICreateTodoData {
+  text: string;
+}
+
+export class TodosRepository {
+  private ormRepository: Repository<TodoModel>;
+
+  constructor(connection: Connection) {
+    this.ormRepository = connection.getRepository(TodoModel);
+  }
+
+  public async create({ text }: ICreateTodoData): Promise<TodoModel> {
+    const todo = this.ormRepository.create({
+      text,
+      is_toggled: false,
+    });
+
+    await this.ormRepository.save(todo);
+
+    return todo;
+  }
+
+  public async toggle(id: number): Promise<void> {
+    await this.ormRepository.query(
+      'UPDATE todos SET status = ((status | 1) - (status & 1)) WHERE id = ?;',
+      [id],
+    );
+  }
+
+  public async delete(id: number): Promise<void> {
+    await this.ormRepository.delete(id);
+  }
+}
